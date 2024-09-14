e18f7ee5c62f8751ffa18dc5711b70d6eee01825
pyr33x <pyrexrj@gmail.com>
2024-09-14 15:11:31 +0330

    feat: suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,41 @@
 import { Button, Input, Separator } from "~/components/ui";
 import { notes } from "~/server/db/schema";
+import { Skeleton } from "~/components/ui";
 import { submit } from "~/actions";
 import { sql } from "drizzle-orm";
 import { auth } from "~/lib/auth";
 import { db } from "~/server/db";
+import { Suspense } from "react";
 
-export default async function HomePage({
-  searchParams,
-}: {
-  searchParams: { asc: string };
-}) {
-  const session = await auth();
+async function Notes({ searchParams }: { searchParams?: { asc: string } }) {
   const nts = await db
     .select()
     .from(notes)
     .limit(5)
-    .orderBy(searchParams.asc ? sql`${notes.id} ASC` : sql`${notes.id} DESC`);
+    .orderBy(searchParams?.asc ? sql`${notes.id} ASC` : sql`${notes.id} DESC`);
   return (
-    <section className="max-w-4xl selection:bg-secondary selection:text-foreground">
+    <ul>
+      {nts.map((note) => (
+        <li
+          key={note.id}
+          className="my-2 rounded-[8px] border-neutral-800 bg-neutral-900 px-4 py-1 text-xl text-foreground/90"
+        >
+          {note.text}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default async function HomePage() {
+  const session = await auth();
+  return (
+    <section className="max-w-4xl">
       <h1 className="text-wrap bg-gradient-to-b from-white to-background bg-clip-text text-center text-5xl font-black tracking-tight text-transparent lg:text-7xl">
         Welcome to Quik
       </h1>
       {session ? (
-        <p className="mt-2 text-center text-lg font-medium text-muted-foreground">
+        <p className="mt-2 text-center text-base font-medium text-muted-foreground md:text-lg">
           You successfully signed into your account!
         </p>
       ) : (
@@ -48,16 +61,9 @@ export default async function HomePage({
         </Button>
       </form>
       <div className="mt-5">
-        <ul>
-          {nts.map((note) => (
-            <li
-              key={note.id}
-              className="my-2 rounded-[8px] border-neutral-800 bg-neutral-900 px-4 py-1 text-xl text-foreground/90"
-            >
-              {note.text}
-            </li>
-          ))}
-        </ul>
+        <Suspense fallback={<Skeleton className="h-10" />}>
+          <Notes />
+        </Suspense>
       </div>
     </section>
   );
